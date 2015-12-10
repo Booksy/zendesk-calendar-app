c1c547bc285c5c073bce2b40648a5211a3070bb1
Dominik Miedziński <dominik@mdzn.pl>
2015-12-10 09:59:33 +0100

    Use date fields instead of text

--- app.js
+++ app.js
@@ -9,6 +9,8 @@
         end_time: 'custom_field_24500385'
     };
 
+    var can_sync = false;
+
     return {
         events: {
             'app.activated': 'init',
@@ -29,17 +31,30 @@
             ticket.customField(fields.end_time, data.end_time);
         },
 
-        send_data: function() {
+        datefield_to_iso: function(field) {
             var ticket = this.ticket();
+            var date = moment(Date.parse(ticket.customField(field))).format('YYYY-MM-DD');
+            return date !== 'Invalid date' ? date : null;
+        },
+
+        send_data: function() {
+            // make sure the app.registered has fired
+            if(!can_sync) {
+                return;
+            }
+
+            var ticket = this.ticket();
+
             this.postMessage('datetime', {
-                start_date: ticket.customField(fields.start_date),
+                start_date: this.datefield_to_iso(fields.start_date),
                 start_time: ticket.customField(fields.start_time),
-                end_date: ticket.customField(fields.end_date),
+                end_date: this.datefield_to_iso(fields.end_date),
                 end_time: ticket.customField(fields.end_time)
             });
         },
 
         ticket_location: function() {
+            can_sync = true;
             // https://github.com/zendesk/zendesk_app_framework_sdk/issues/7
             var location = this.currentLocation();
             if(location === 'new_ticket_sidebar') {
